f773a1e9bd6c11c0604403445f483c587d5ec713
yagnik1312 <yagnik054@gmail.com>
2026-04-09 14:38:04 +0530

    feat: day 1

---
+++ src/main/java/com/example/model/Product.java
@@ -0,0 +1,12 @@
+package com.example.model;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+}
